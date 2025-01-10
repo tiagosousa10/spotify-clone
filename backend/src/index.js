ce7a53c08d0224424b9e658cf99a1306e78f0a09
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-10 21:34:12 +0100

    connect to DB with mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import { connectDB } from "./lib/db.js";
+
 import userRoutes from './routes/user.route.js'
 import adminRoutes from './routes/admin.route.js'
 import authRoutes from './routes/auth.route.js'
 import songRoutes from './routes/song.route.js'
 import albumRoutes from './routes/album.route.js'
-import statsRoutes from './routes/stats.route.js'
+import statRoutes from './routes/stats.route.js'
 
 dotenv.config();
 
@@ -14,12 +16,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/users", userRoutes) //users using userRoutes
-app.use("/api/auth", authRoutes) //auth using authRoutes
 app.use("/api/admin", adminRoutes) //admin using adminRoutes
+app.use("/api/auth", authRoutes) //auth using authRoutes
 app.use("/api/songs", songRoutes) //songs using songRoutes
 app.use("/api/albums", albumRoutes) //albums using albumRoutes
-app.use("/api/stats", statsRoutes) //stats using statsRoutes
+app.use("/api/stats", statRoutes) //stats using statsRoutes
 
 app.listen(PORT, () => {
   console.log("nodemon. Server is running on port " + PORT)
+  connectDB() //connect to database
 })
