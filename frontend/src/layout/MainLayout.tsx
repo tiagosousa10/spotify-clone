5dff4510931b83bd78e4af97aba6c6c2d5d93f52
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-12 01:41:41 +0100

    starting MainLayout with resizable from clerk

---
+++ frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,28 @@
+import { ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
+import { Outlet } from "react-router-dom"
+
+const MainLayout = () => {
+   const isMobile = false
+   return (
+    <div className="h-screen bg-black text-white flex flex-col">
+      <ResizablePanelGroup direction="horizontal" className="flex-1 flex h-full overflow-hidden p-2">
+         {/*left sidebar */}
+         <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
+            left sidebar
+         </ResizablePanel>
+         
+         {/*main content */}
+         <ResizablePanel defaultSize={isMobile ? 80 : 60}  >
+            <Outlet/>
+         </ResizablePanel>
+
+         {/*   right sidebar */}
+         <ResizablePanel defaultSize={20} minSize={0} maxSize={25} collapsedSize={0}>
+            friend activity 
+         </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  )
+}
+
+export default MainLayout
