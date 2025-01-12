023ab7929b9c81b66f101bae211a1612db16615a
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-12 19:15:44 +0100

    fetching album by id

---
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AlbumPage = () => {
+  return (
+    <div>AlbumPage</div>
+  )
+}
+
+export default AlbumPage
--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -1,20 +1,26 @@
 import { axiosInstance } from '@/lib/axios';
 import {create} from 'zustand';
-import {Song, Album} from '@/types'
+import {Song, Album} from '@/types' // @ means src
 
 interface MusicStore {
    songs: Song[], //imported from types
    albums: Album[], //imported from types
-   isLoading:boolean,
+   isLoading: boolean,
    error: string | null,
-   fetchAlbums: () => Promise<void>
+   currentAlbum: null
+
+   fetchAlbums: () => Promise<void>,
+   fetchAlbumById: (id:string) => Promise<void>,
 }
+
 
 export const useMusicStore = create<MusicStore>((set) =>({
    songs: [],
    albums: [],
    isLoading : false,
    error:null,
+   currentAlbum: null,
+
 
    fetchAlbums: async () => {
       set({isLoading: true, error:null})
@@ -29,6 +35,20 @@ export const useMusicStore = create<MusicStore>((set) =>({
 
       } finally {
          set({isLoading: false})
+      }
+   },
+
+
+   fetchAlbumById: async (id:string) => {
+     set({isLoading:true, error:null})
+      try {
+         const response = await axiosInstance.get(`/albums/${id}`) //get album by id
+         set({currentAlbum: response.data})
+
+      } catch(error: any) {
+         set({error: error.response.data.message})
+      } finally {
+         set({isLoading:false})
       }
    }
 }))
