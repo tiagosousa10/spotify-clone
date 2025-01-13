730dc4a55564fb990c46671c1b77371b4e879b4d
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-13 16:22:26 +0100

    adding featured grid skeleton

--- frontend/src/pages/home/components/FeaturedSection.tsx
+++ frontend/src/pages/home/components/FeaturedSection.tsx
@@ -1,6 +1,10 @@
+import { useMusicStore } from '@/stores/useMusicStore'
 import React from 'react'
 
 const FeaturedSection = () => {
+   const {isLoading, featuredSongs, error} = useMusicStore()
+   if(isLoading) return <FeaturedGridSkeleton/>
+
   return (
     <div>FeaturedSection</div>
   )
