9412e23991aa5276b66f44c63639f01ffb699b17
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-14 16:37:08 +0100

    initialize queu, play album  and set current song to player store

--- frontend/src/stores/usePlayerStore.ts
+++ frontend/src/stores/usePlayerStore.ts
@@ -17,14 +17,49 @@ interface PlayerStore {
 }
 
 export const usePlayerStore = create<PlayerStore>((set,get) => ({
+//initial state
    currentSong: null,
    isPlaying: false,
    queue: [],
    currentIndex: -1,
 
-   initializeQueue: (songs: Song[]) => {},
-   playAlbum:(songs: Song[], startIndex?: number) => {},
-   setCurrentSong: (song: Song | null) => {},
+   initializeQueue: (songs: Song[]) => {
+      set({
+         queue:songs, 
+         currentSong: get().currentSong || songs[0],
+         currentIndex: get().currentIndex === -1 ? 0 : get().currentIndex
+      })
+   },
+
+
+   playAlbum:(songs: Song[], startIndex=0) => {
+      if(songs.length === 0) return //if no songs, return
+
+      const song = songs[startIndex] //get current song on position 0 from the list of songs
+
+      set({
+         queue: songs, 
+         currentSong: song,
+         currentIndex: startIndex,
+         isPlaying:true
+      })
+
+   },
+
+
+   setCurrentSong: (song: Song | null) => {
+      if(!song) return;
+
+      const songIndex = get().queue.findIndex((s) => s._id === song._id) //find song in queue
+
+      set({
+         currentSong: song,
+         isPlaying:true,
+         currentIndex: songIndex !== -1 ? songIndex : get().currentIndex
+      })
+   },
+
+
    togglePlay: () => {},
    playNext: () => {},
    playPrevious: () => {}
