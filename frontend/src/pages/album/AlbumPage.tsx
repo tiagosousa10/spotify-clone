b54bb1cb1d9c786765204fcbc33a2407ab28c951
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-14 22:22:44 +0100

    puted audioplayer in album page

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -89,7 +89,37 @@ const AlbumPage = () => {
                {/* songs list*/}
                <div className='px-6'>
                   <div className='space-y-2 py-4'>
-                    )}
+                     {currentAlbum?.songs.map((song,index) => {
+                        const isCurrentSong = currentSong?._id === song.id //check if song is current song
+
+                        return (
+                        <div 
+                           key={song._id} 
+                           onClick={() => handlePlayAlbum(index)}
+                           className={`grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-4 py-2 text-sm 
+                              text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer
+                           `}>
+                              <div className='flex items-center justify-center'> 
+                                 {isCurrentSong && isPlaying ? (
+                                    <div className='size-4 text-green-500'>♫</div>
+                                 ) : (
+                                    <span className='group-hover:hidden'>{index + 1}</span>
+                                 )}
+                                 <Play className='h-4 w-4 hidden group-hover:block'   />
+                              </div>
+
+                              <div className='flex items-center gap-3'>
+                                 <img src={song.imageUrl} alt={song.title} className='size-10' />
+
+                                 <div>
+                                    <div className='font-medium text-white'> {song.title} </div>
+                                    <div>{song.artist}</div>
+                                 </div>
+                              </div>
+                              <div className='flex items-center'>{song.createdAt.split("T")[0]}</div>
+                              <div className='flex items-center'>{formatDuration(song.duration)}</div>
+                        </div>
+                     )})}
                   </div>
                </div>
                </div>
