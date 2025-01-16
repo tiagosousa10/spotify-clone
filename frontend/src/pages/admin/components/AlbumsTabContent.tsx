79972599dc3228de912f351336a409f755c01957
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-17 00:24:13 +0100

    Albums content table added

--- frontend/src/pages/admin/components/AlbumsTabContent.tsx
+++ frontend/src/pages/admin/components/AlbumsTabContent.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Library } from 'lucide-react'
+import AlbumsTable from './AlbumsTable'
 
 const AlbumsTabContent = () => {
   return (
-    <div>AlbumsTabContent</div>
+    <Card className="bg-zinc-800/50 border-zinc-700/50">
+      <CardHeader>
+              <div className='flex items-center justify-between'>
+                <div>
+                  <CardTitle className='flex items-center gap-2'>
+                    <Library className='h-5 w-5 text-violet-500' />
+                    Albums Library
+                  </CardTitle>
+                  <CardDescription>Manage your album collection</CardDescription>
+                </div>
+                <Button>+</Button>
+              </div>
+            </CardHeader>
+
+            <CardContent>
+              <AlbumsTable/>
+            </CardContent>
+    </Card>
   )
 }
 
